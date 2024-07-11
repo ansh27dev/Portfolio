a385a75696770c90458e78f05d451d03fe2b0949
ansh27dev <ansh255gupta@gmail.com>
2024-07-11 18:04:03 +0530

    visibility modification in ui

--- client/src/components/second.jsx
+++ client/src/components/second.jsx
@@ -79,78 +79,80 @@ const second = () => {
         <br />
         <br />
         <div className="slider">
-          <div className="slide-track">
-            <div className="slide">
-              <img
-                src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
-                alt=""
-              />
-            </div>
-            <div className="slide">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
-                alt=""
-              />
-            </div>
-            <div className="slide">
-              <img
-                src="https://profilinator.rishav.dev/skills-assets/java-original-wordmark.svg"
-                alt=""
-              />
-            </div>
-            <div className="slide">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
-                alt=""
-              />
-            </div>
-            <div className="slide">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
-                alt=""
-              />
-            </div>
-            <div className="slide">
-              <img
-                src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
-                alt=""
-              />
-            </div>
-            <div className="slide">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
-                alt=""
-              />
-            </div>
-            <div className="slide">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-                alt=""
-              />
-            </div>
-            <div className="slide">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
-                alt=""
-              />
-            </div>
-            <div className="slide">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
-                alt=""
-              />
-            </div>
-            <div className="slide">
-              <img
-                src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
-                alt=""
-              />
-            </div>
-            <div className="slide">
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
-                alt=""
-              />
+          <div className="slide-wrap">
+            <div className="slide-track">
+              <div className="slide">
+                <img
+                  src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
+                  alt=""
+                />
+              </div>
+              <div className="slide">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
+                  alt=""
+                />
+              </div>
+              <div className="slide">
+                <img
+                  src="https://profilinator.rishav.dev/skills-assets/java-original-wordmark.svg"
+                  alt=""
+                />
+              </div>
+              <div className="slide">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+                  alt=""
+                />
+              </div>
+              <div className="slide" id="express">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+                  alt=""
+                />
+              </div>
+              <div className="slide">
+                <img
+                  src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+                  alt=""
+                />
+              </div>
+              <div className="slide">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+                  alt=""
+                />
+              </div>
+              <div className="slide">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                  alt=""
+                />
+              </div>
+              <div className="slide">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+                  alt=""
+                />
+              </div>
+              <div className="slide">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+                  alt=""
+                />
+              </div>
+              <div className="slide">
+                <img
+                  src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+                  alt=""
+                />
+              </div>
+              <div className="slide">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+                  alt=""
+                />
+              </div>
             </div>
           </div>
         </div>
